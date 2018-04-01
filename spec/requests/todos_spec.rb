17ecfa1f0d32f04bac514121034238cf84ce7f5f
karuna24s <karuna24s@gmail.com>
2018-04-01 17:42:41 -0400

    implement user model and specs

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -21,7 +21,6 @@ RSpec.describe 'Todos API', type: :request do
     before { get "/todos/#{todo_id}" }
 
     context 'when the record exists' do
-
       it 'returns the todo' do
         expect(json).not_to be_empty
         expect(json['id']).to eq(todo_id)
@@ -41,6 +40,35 @@ RSpec.describe 'Todos API', type: :request do
 
       it 'returns a not found message' do
         expect(response.body).to match(/Couldn't find Todo/)
+      end
+    end
+  end
+
+  describe 'POST /todos' do
+    let(:valid_attributes) { { title: 'Learn Elm', created_by: '1' } }
+
+    context 'when request is valid' do
+      before { post '/todos', params: valid_attributes }
+
+      it 'creates a todo' do
+        expect(json['title']).to eq('Learn Elm')
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when request is invalid' do
+      before { post '/todos', params: { title: 'Foobar' } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match(/Validation failed: Created by can't be blank/)
       end
     end
   end
